312363875ed9642dc8e8e71db9dd601550c3b79a
Blake Smith <blakeinvictoria@gmail.com>
2020-11-25 09:33:23 -0800

    Copy paths to test files from virtool.jobs.pathoscope.
    Re-factor to use pathlib over os.join.

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,3 +1,22 @@
+from pathlib import Path
+
+TEST_FILES = Path(__file__).parent / "test_files"
+
+BEST_HIT_PATH = TEST_FILES / "best_hit"
+RESULTS_PATH = TEST_FILES / "results.json"
+EM_PATH = TEST_FILES / "em"
+ISOLATES_VTA_PATH = TEST_FILES / "to_isolates.vta"
+MATRIX_PATH = TEST_FILES / "ps_matrix"
+REF_LENGTHS_PATH = TEST_FILES / "ref_lengths.json"
+SAM_PATH = TEST_FILES / "test_al.sam"
+SCORES = TEST_FILES / "scores"
+TO_SUBTRACTION_PATH = TEST_FILES / "to_subtraction.json"
+UNU_PATH = TEST_FILES / "unu"
+VTA_PATH = TEST_FILES / "test.vta"
+INDEXES_PATH = TEST_FILES /"index"
+FASTQ_PATH = TEST_FILES / "test.fq"
+HOST_PATH = INDEXES_PATH / "host"
+
 def test_map_default_isolates():
     assert False
 
